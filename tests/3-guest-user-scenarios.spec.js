07796ed802fe179a4b92b1a8806a1c1de3cbb0f2
tanayamasonautomation2024 <tanaya.pai@changecx.com>
2025-03-05 13:08:35 +0530

    5Mar2025

--- tests/3-guest-user-scenarios.spec.js
+++ tests/3-guest-user-scenarios.spec.js
@@ -1,7 +1,9 @@
 import { test, expect, devices } from '@playwright/test';
-import { CasePage } from '../pages/check-for-qualification';
+import { CasePage } from '../pages/check-for-qualification.js';
 import { ESignAgreementPage } from '../pages/esign-agreement-page.js';
 import testData from '../test_data/qualification_case_details.json' assert { type: 'json' };
+
+let email='';
 
 test.describe.serial('Not-LoggedIn User Scenarios', () => {
   test.setTimeout(150000);
@@ -14,8 +16,7 @@ test.describe.serial('Not-LoggedIn User Scenarios', () => {
     test.slow();
     try {
       //await page.goto(process.env.WEB_URL);
-      await page.goto(process.env.LANTERN_URL);
-      await page.waitForTimeout(3000);
+      
       // const homePage = new HomePage(page);
       // await homePage.beTheFirstToKnow();
     } catch (error) {
@@ -28,11 +29,13 @@ test.describe.serial('Not-LoggedIn User Scenarios', () => {
     const qualifiedCasePage = new CasePage(page);
     const eSignDocumentPage = new ESignAgreementPage(page);
    // await qualifiedCasePage.navigateToHomePage();
+    await page.goto(process.env.LANTERN_URL);
+    await page.waitForTimeout(3000);
     await qualifiedCasePage.closeCookieBanner();
     await qualifiedCasePage.searchAndOpenCase(testData.caseName);
     await qualifiedCasePage.startQualification();
     await qualifiedCasePage.fillPersonalDetails(testData.firstName, testData.lastName, testData.dropdownSelection_yes);
-    const email = await qualifiedCasePage.fillContactInfo(testData.emailforqualified, testData.phone);
+    email = await qualifiedCasePage.fillContactInfo(testData.emailforqualified, testData.phone);
     await qualifiedCasePage.fillAddress(testData.address, testData.autosuggestadd, testData.addressline1, testData.city, testData.zip);
     await qualifiedCasePage.submitForm();
     await qualifiedCasePage.verifySuccessMessage();
@@ -41,6 +44,13 @@ test.describe.serial('Not-LoggedIn User Scenarios', () => {
    // await eSignDocumentPage.completeSignReleaseProcess();
   });
 
+test.describe("Admin Scenario", () => {
+    test.use({ storageState: './adminUser1.json' });
+    test('Admin Scenario', async ({ page }) => {
+      await page.goto(process.env.LANTERN_ADMIN_URL);
+      await page.waitForTimeout(10000);
+    })
+    });
  
 // **After each test: log success or failure**
 test.afterEach(async ({ page }, testInfo) => {
